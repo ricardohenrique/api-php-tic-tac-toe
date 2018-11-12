6c30a87d4fcd780069364de690c0d13d8f08e9d6
Ricardo Mota <ricardo.mota@ftd.com.br>
2018-11-12 18:23:50 -0200

    added test to illegal movement and home page

--- tests/Integration/MatchTest.php
+++ tests/Integration/MatchTest.php
@@ -12,6 +12,17 @@ class MatchTest extends Setup
     use TicTacToeSupport;
 
     /**
+     * Test to see index
+     *
+     * @return void
+     */
+    public function testSeeIndex()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    /**
      * Test to get all matches
      *
      * @return void
@@ -61,17 +72,37 @@ class MatchTest extends Setup
             $match = factory(Matches::class)->create();
 
             while ($validateMatch == false) {
-                $responseGetMatch = Matches::find($match->id);
-                $board = $responseGetMatch->board;
-                $movement = $this->getMovement($board);
+                $matchModel = Matches::find($match->id);
+                $movement = $this->getMovement($matchModel->board);
 
                 $response = $this->put('/api/match/'.$match->id, ["position" => $movement]);
-                $jsonResponse = json_decode($response->getContent());
                 if ($response->getStatusCode() == 400) {
                     $response->assertStatus(400);
                     $response->assertJson(["match-already-finished"]);
                     $validateMatch = true;
                 }
+            }
+        }
+    }
+
+    /**
+     * Test to play match with illegal movement
+     *
+     * @return void
+     */
+    public function testPlayMatchIllegalMovement()
+    {
+        $validateMatch = false;
+        $match = factory(Matches::class)->create();
+
+        $matchModel = Matches::find($match->id);
+        $movement = $this->getMovement($matchModel->board);
+        while ($validateMatch == false) {
+            $response = $this->put('/api/match/'.$match->id, ["position" => $movement]);
+            if ($response->getStatusCode() == 400) {
+                $response->assertStatus(400);
+                $response->assertJson(["illegal-movement"]);
+                $validateMatch = true;
             }
         }
     }
@@ -88,5 +119,20 @@ class MatchTest extends Setup
         $response->assertStatus(200);
         $response->assertJsonStructure([$this->getStructureMatch()]);
         $response->assertJsonMissing([$match->name]);
+    }
+
+
+    /**
+     * Test to delete match
+     *
+     * @return void
+     */
+    public function testCommandMatch()
+    {
+        $this->artisan('php artisan tictactoe:play')
+         // ->expectsQuestion('What is your name?', 'Taylor Otwell')
+         // ->expectsQuestion('Which language do you program in?', 'PHP')
+         // ->expectsOutput('Your name is Taylor Otwell and you program in PHP.')
+         ->assertExitCode(0);
     }
 }
