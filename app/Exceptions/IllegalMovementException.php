a3c179b00c4e7b3cd02d50d252beaa54b5d53441
Ricardo Mota <ricardo.mota@ftd.com.br>
2018-11-12 13:55:02 -0200

    removed Log

--- app/Exceptions/IllegalMovementException.php
+++ app/Exceptions/IllegalMovementException.php
@@ -3,7 +3,6 @@
 namespace App\Exceptions;
 
 use Exception;
-use Log;
 
 class IllegalMovementException extends Exception
 {
--- app/Exceptions/MatchAlreadyFinishedException.php
+++ app/Exceptions/MatchAlreadyFinishedException.php
@@ -3,7 +3,6 @@
 namespace App\Exceptions;
 
 use Exception;
-use Log;
 
 class MatchAlreadyFinishedException extends Exception
 {
